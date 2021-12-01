0e30004604e8ca20afffa5da59cfec3f45634983
vincent <kwsomg@163.com>
2021-12-01 22:39:46 +0800

    page object

---
+++ page/article.py
@@ -0,0 +1,32 @@
+# /Users/kongweicheng/desktop
+# encoding : utf-8
+# system : macbook pro
+'''
+@author:David
+@time:2021/12/110:33 下午
+@DESC : 
+'''
+
+
+class AllArticle:
+
+    def is_jump(self):
+        ...
+
+    def check(self):
+        ...
+
+    def add(self):
+        ...
+
+
+class WriteArticle:
+    ...
+
+
+class KindList:
+    ...
+
+
+class Tag:
+    ...
---
+++ page/dashboard.py
@@ -0,0 +1,8 @@
+# /Users/kongweicheng/desktop
+# encoding : utf-8
+# system : macbook pro
+'''
+@author:David
+@time:2021/12/110:32 下午
+@DESC : 
+'''
